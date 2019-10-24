cf910ee4a397fbbdec2656050460ceac79102496
deorsi <deorsi@protonmail.com>
2019-10-24 11:47:51 -0300

    minor change to connect database

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://sm4rtbrain.herokuapp.com/signin', {
+    fetch('https://sm4rtbrain-api.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
